95caca98f693db22670828a24e5895795e2bca36
Maria <workmail358@gmail.com>
2024-02-12 23:30:45 +0300

    feat: solve task getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -388,8 +388,10 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const one = arr.map((el) => Boolean(el));
+  const res = one.filter((el) => el === false);
+  return res.length;
 }
 
 /**
